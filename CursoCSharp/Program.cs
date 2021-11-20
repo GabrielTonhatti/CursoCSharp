0375b05ffb4cb04ef4235bbe5e7eecb7bdbb9a2a
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-11-20 16:38:17 -0300

    Operadores Relacionais

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-
 using CursoCSharp.Fundamentos;
 
-namespace CursoCSharp {
-    class Program {
-        static void Main(string[] args) {
-            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>()
+            {
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
